99abebf206c389c634fcdf5cff98ec48d166743e
genecc15 <genecc15@gmail.com>
2020-05-02 02:34:57 -0600

    Inicio Huffman

---
+++ LabReposicion/ArbolBinario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabReposicion
+{
+    public class ArbolBinario
+    {
+        public Nodo Raiz { get; set; }
+
+        public bool esVacio { get { return Raiz == null; } }
+
+        //Constructor 
+        public ArbolBinario() { }
+    }
+}
